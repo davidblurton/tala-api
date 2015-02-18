219a6da5b34bf2db185927830bd9baf795fffd0e
David Blurton <david.blurton@gmail.com>
2015-02-18 10:41:45 +0000

    Use object literal syntax

--- database.js
+++ database.js
@@ -2,19 +2,25 @@ var level = require('level')
 var db = level(process.cwd() + '/db')
 
 export default {
-  search: prefix => db.createKeyStream({
-    gte: prefix,
-    lt: prefix + '\xff'
-  }),
+  search(prefix) {
+    return db.createKeyStream({
+      gte: prefix,
+      lt: prefix + '\xff'
+    })
+  },
 
-  find: word => db.createKeyStream({
-    gte: word + '~',
-    lt: word + '~' + '\xff'
-  }),
+  find(word) {
+    return db.createKeyStream({
+      gte: word + '~',
+      lt: word + '~' + '\xff'
+    })
+  },
 
-  findOne: word => db.createKeyStream({
-    gte: word + '~',
-    lt: word + '~' + '\xff',
-    limit: 1
-  })
+  findOne(word) {
+    return db.createKeyStream({
+      gte: word + '~',
+      lt: word + '~' + '\xff',
+      limit: 1
+    })
+  }
 }
