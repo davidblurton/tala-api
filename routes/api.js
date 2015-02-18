8b065b301725eb2181cdee265f5f81239078489c
David Blurton <david.blurton@gmail.com>
2015-02-18 00:09:42 +0000

    Use catch for error handling

--- routes/api.js
+++ routes/api.js
@@ -4,37 +4,44 @@ var format = require('./result-formatter');
 
 router.get('/:word', (req, res, next) => {
   word.lookup(req.params.word)
-    .then(results => res.send(format(results, req.query.lang)), next);
+    .then(results => res.send(format(results, req.query.lang)))
+    .catch(next)
 });
 
 router.get('/:word/related', (req, res, next) => {
   word.related(req.params.word)
-    .this(results => res.send(format(results, req.query.lang)), next);
+    .this(results => res.send(format(results, req.query.lang)))
+    .catch(next)
 });
 
 router.get('/:word/tags/', (req, res, next) => {
   word.tags(req.params.word)
-    .then(results => res.send(format(results, req.query.lang)), next);
+    .then(results => res.send(format(results, req.query.lang)))
+    .catch(next)
 });
 
 router.get('/:word/tag/:tags', (req, res, next) => {
   word.tag(req.params.word, req.params.tags)
-    .then(results => res.send(format(results, req.query.lang)), next);
+    .then(results => res.send(format(results, req.query.lang)))
+    .catch(next)
 });
 
 router.get('/:prefix/prefix', (req, res, next) => {
   word.prefix(req.params.prefix)
-    .then(results => res.send(format(results, req.query.lang)), next);
+    .then(results => res.send(format(results, req.query.lang)))
+    .catch(next)
 });
 
 router.get('/:prefix/suggestions', (req, res, next) => {
   word.suggestions(req.params.prefix, req.query.limit)
-    .then(results => res.send(format(results)), next);
+    .then(results => res.send(format(results)))
+    .catch(next)
 });
 
 router.get('/:prefix/fuzzy', (req, res, next) => {
   word.fuzzy(req.params.prefix)
-    .then(results => res.send(format(results, req.query.lang)), next);
+    .then(results => res.send(format(results, req.query.lang)))
+    .catch(next)
 });
 
 module.exports = router;
