5ccc702ef9b1be826a11ada6923c408b708e59f1
David Blurton <david.blurton@gmail.com>
2015-02-18 10:41:37 +0000

    Require server so we can use ES6 in it

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-require("babel/register");
-
 var express = require('express');
+var api = require('./routes/api');
+var index = require('./routes/index');
+
 var app = express();
 app.set('views', __dirname + '/app/views');
 app.set('view engine', 'jade')
-
-var api = require('./routes/api');
-var index = require('./routes/index');
 
 app.use('/api', api);
 app.use('/', index);
 app.use(express.static(__dirname + '/app'));
 
-var server = app.listen(8000, function () {
+var server = app.listen(8000, () => {
   var host = server.address().address;
   var port = server.address().port;
 
