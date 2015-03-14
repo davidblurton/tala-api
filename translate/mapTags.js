74424b4b3bd37edda20c380967df346d41553ffc
David Blurton <david.blurton@gmail.com>
2015-03-14 13:57:16 +0000

    Group tags

--- translate/mapTags.js
+++ translate/mapTags.js
@@ -2,19 +2,32 @@ var matchers = require('./tags');
 
 export default (tags, wordClass) => {
   return {
-    masculine: matchers.isMasculine(tags, wordClass),
-    feminine: matchers.isFeminine(tags, wordClass),
-    neuter: matchers.isNeuter(tags, wordClass),
-    singular: matchers.isSingular(tags),
-    plural: matchers.isPlural(tags),
-    definite: matchers.isDefinite(tags),
-    indefinite: matchers.isIndefinite(tags, wordClass),
-    nominitive: matchers.isNominative(tags),
-    accusative: matchers.isAccusative(tags),
-    genitive: matchers.isGenitive(tags),
-    dative: matchers.isDative(tags),
-    "first person": matchers.isFirstPerson(tags),
-    "second person": matchers.isSecondPerson(tags),
-    "third person": matchers.isThirdPerson(tags)
+    case: {
+      nominitive: matchers.isNominative(tags),
+      accusative: matchers.isAccusative(tags),
+      genitive: matchers.isGenitive(tags),
+      dative: matchers.isDative(tags)
+    },
+    number: {
+      singular: matchers.isSingular(tags),
+      plural: matchers.isPlural(tags)
+    },
+    article: {
+      definite: matchers.isDefinite(tags),
+      indefinite: matchers.isIndefinite(tags, wordClass)
+    },
+    gender: {
+      masculine: matchers.isMasculine(tags),
+      feminine: matchers.isFeminine(tags),
+      neuter: matchers.isNeuter(tags)
+    },
+    person: {
+      "first person": matchers.isFirstPerson(tags),
+      "second person": matchers.isSecondPerson(tags),
+      "third person": matchers.isThirdPerson(tags)
+    },
+    tense: {
+
+    }
   }
 }
