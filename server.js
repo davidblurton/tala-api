b855ed3fb5840aadbe058c11b8802b00b0b6f8c8
David Blurton <david.blurton@gmail.com>
2015-03-15 18:18:23 +0000

    Add response time

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-var express = require('express');
-var api = require('./routes/api');
-var index = require('./routes/index');
-var cors = require('cors');
+var express = require('express')
+var api = require('./routes/api')
+var index = require('./routes/index')
+var cors = require('cors')
+var responseTime = require('response-time')
 
-var app = express();
-app.set('views', __dirname + '/app/views');
+var app = express()
+app.set('views', __dirname + '/app/views')
 app.set('view engine', 'jade')
 
-app.use(cors());
-app.use('/api', api);
-app.use('/', index);
-app.use(express.static(__dirname + '/app'));
+app.use(cors())
+app.use(responseTime())
+
+app.use('/api', api)
+app.use('/', index)
+app.use(express.static(__dirname + '/app'))
 
 var server = app.listen(8000, () => {
-  var host = server.address().address;
-  var port = server.address().port;
+  var host = server.address().address
+  var port = server.address().port
 
-  console.log('app running at http://%s:%s', host, port);
-});
+  console.log('app running at http://%s:%s', host, port)
+})
