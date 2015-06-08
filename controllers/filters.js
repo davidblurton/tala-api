c6067346fbbaafaabb65407f093a69cce469a5f5
David Blurton <david.blurton@gmail.com>
2015-06-08 13:47:04 +0000

    Bad lines

--- controllers/filters.js
+++ controllers/filters.js
@@ -4,8 +4,6 @@ export default {
   },
 
   includes(array, property, values) {
-    return array.filter(values ? x =>
-      values.every(value => x[property].includes(value))
-    : true)
+    return array.filter(values ? x => values.every(value => x[property].includes(value)) : true)
   }
 }
