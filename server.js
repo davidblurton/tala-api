0e0dcef7219080ba51a2d54afb53ea7847cdb49d
David Blurton <david.blurton@gmail.com>
2015-07-19 16:14:07 +0000

    Tidy up server file

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import cors from 'cors'
+import responseTime from 'response-time'
+
 import api from './routes/api'
 import summary from './routes/summary'
 import index from './routes/index'
-import cors from 'cors'
-import responseTime from 'response-time'
 
 let app = express()
 
@@ -13,11 +14,10 @@ app.use(responseTime())
 app.use('/', api)
 app.use('/', summary)
 app.use('/', index)
-app.use(express.static(__dirname + '/app'))
 
 let server = app.listen(8000, () => {
   let host = server.address().address
   let port = server.address().port
 
-  console.log('app running at http://%s:%s', host, port)
+  console.log(`app running at http://${host}:${port}`)
 })
