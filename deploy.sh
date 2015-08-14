6a93dbb3d73afc0313228257dcd8655166d2a697
David Blurton <david.blurton@gmail.com>
2015-08-14 22:00:36 +0000

    Run tests before deploying

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
-eval "$(boot2docker shellinit)"
-docker build -t davidblurton/tala-api . && docker push davidblurton/tala-api
+if npm run test; then
+  eval "$(boot2docker shellinit)"
+  docker build -t davidblurton/tala-api . && docker push davidblurton/tala-api
+fi
