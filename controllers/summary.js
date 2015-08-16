9cb2d647acd8c3d0dfbdc70b865f174535e9c151
David Blurton <david.blurton@gmail.com>
2015-08-16 13:20:56 +0000

    Add more error handling

--- controllers/summary.js
+++ controllers/summary.js
@@ -4,17 +4,6 @@ import getPrepositionFilters from '../filters/prepositions'
 import getVerbFilters from '../filters/verbs'
 import getAdjectiveFilters from '../filters/adjectives'
 
-function includes(array, values) {
-  return Object.keys(values).map(key => {
-    let result = {}
-    let value = values[key]
-
-    result[key] = array.filter(x => x.grammarTag === value)[0]
-
-    return result
-  })[0]
-}
-
 // Generates a list of autocompletion suggestions.
 function suggestions(prefix, limit) {
   return database.search(prefix)
@@ -22,15 +11,6 @@ function suggestions(prefix, limit) {
     .then(results => results.map(x => x.wordForm))
     .then(_.unique)
     .then(results => results.slice(0, limit))
-}
-
-// Are there matching words from multiple headwords.
-function multiple(word) {
-  return database.lookup(word)
-    .then(results => {
-      let ids = results.map(result => result.binId)
-      return !ids.every(id => id === ids[0])
-    })
 }
 
 // Find all words from the same headword.
