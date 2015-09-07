0e03af160bac8aee233a76a05fb463c1525c3091
David Blurton <david.blurton@gmail.com>
2015-09-07 22:26:05 +0000

    Use predeploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
-if npm run test; then
-  eval "$(boot2docker shellinit)"
-  docker build -t davidblurton/tala-api . && docker push davidblurton/tala-api
-fi
+eval "$(boot2docker shellinit)"
+docker build -t davidblurton/tala-api . && docker push davidblurton/tala-api
