dd4f6b381783f39652f440cfab6ef9bebfff9162
Eirikur Nilsson <eirikur@nilsson.is>
2015-09-10 22:33:29 +0000

    Parse static verb data

---
+++ models/static.js
@@ -0,0 +1,37 @@
+import * as fs from 'fs'
+import Promise from 'prfun'
+
+const readFile = Promise.promisify(fs.readFile)
+
+async function parseStatic() {
+  const data = await readFile('data/verbs.conf', 'utf8')
+  const results = {}
+
+  for (let line of data.split(/[\r\n]+/)) {
+    line = line.replace(/\s*(#.*)?$/, '')
+    const [verb, ...cases] = line.split(/\s+/)
+
+    if (!results[verb]) {
+      results[verb] = []
+    }
+
+    if (cases.length) {
+      // Verb controls cases
+      results[verb].push(...cases)
+    } else {
+      // Verb can be stand-alone
+      results[verb].push('')
+    }
+  }
+
+  return results
+}
+
+const DATA_PROMISE = parseStatic()
+
+async function lookup(verb) {
+  const data = await DATA_PROMISE
+  return data[verb]
+}
+
+export default lookup
---
+++ test/model/static.js
@@ -0,0 +1,24 @@
+import assert from 'assert'
+import lookupVerb from '../../models/static';
+
+const testCases = {
+  'tifa': [''],
+  'tefja': ['', 'þf'],
+  'vernda': ['þf']
+}
+
+describe('Lookup static verb rules', () => {
+  it(`should not include asdf`, async function() {
+    let result = await lookupVerb('asdf')
+    assert.deepEqual(result, null)
+  })
+
+  Object.keys(testCases).forEach(input => {
+    it(`should include ${input}`, async function() {
+      let result = await lookupVerb(input)
+      let expected = testCases[input]
+
+      assert.deepEqual(result, expected)
+    })
+  })
+})
