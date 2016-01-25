93c8280c438ac5bfc7d4459b6a45b11a2420f6a9
David Blurton <david.blurton@gmail.com>
2016-01-25 23:17:37 +0000

    Sort noun results badly

--- formatters/word.js
+++ formatters/word.js
@@ -1,9 +1,37 @@
 import _ from 'lodash'
+import {parse} from '../grammar/parser'
+import lookup from '../translate/translate.json'
+
+function sort(results) {
+  return _.sortBy(results, x => {
+    let rank = {
+      nominative: 1,
+      accusative: 2,
+      dative: 3,
+      genitive: 4,
+    }
+    return rank[x.tags && x.tags.grammarCase]
+  })
+}
+
+function translate(tags, language) {
+  return _.mapValues(tags, tag => lookup[language].grammarTag[tag] || tag)
+}
 
 export default (results, lang) => {
+  results.forEach(result => {
+    try {
+      let tags = parse(result.wordClass, result.grammarTag)
+      result.tags = translate(tags, 'en')
+    } catch(e) {
+      result.tags = {}
+    }
+  })
+
   return {
     count: results.length,
     wordClasses: _.unique(results.map(x => x.wordClass)),
-    results: results
+    results: sort(results),
   }
 }
+
