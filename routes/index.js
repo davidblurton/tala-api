5dcf7508a8657997f5fc692eeaafe461fb9d8dcc
David Blurton <david.blurton@gmail.com>
2016-01-31 00:20:54 +0000

    Add loader.io verification

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,6 @@ let router = new Router()
 
 router.get('/', (req, res) => res.send('tala api running'))
 
+router.get('/loaderio-f7ba020fa41e103dcd582cda31797ceb', (req, res) => res.send('loaderio-f7ba020fa41e103dcd582cda31797ceb'))
+
 export default router
