436c143e6bab4829ce11c540c510321331e2f206
David Blurton <david.blurton@gmail.com>
2016-03-06 01:27:45 +0000

    Catch stats errors

--- middleware/response-time.js
+++ middleware/response-time.js
@@ -2,14 +2,16 @@ import responseTime from 'response-time'
 import StatsD from 'node-statsd'
 
 let stats = new StatsD({
-  host: 'grafana-graphite-45577f49-1.52442bd4.cont.dockerapp.io',
+  host: 'stats.tala.is',
 })
 
 export default responseTime((req, res, time) => {
-  if (req.route && req.route.path) {
+  try {
     let stat = (req.method + req.route.path).toLowerCase()
       .replace(/[:\.]/g, '')
       .replace(/\//g, '_')
     stats.timing(stat, time)
+  } catch (err) {
+    console.log(err)
   }
 })
