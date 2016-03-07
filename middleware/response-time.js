fe607f7690fee1b8e42493a4bf399a06a30a9ff5
David Blurton <david.blurton@gmail.com>
2016-03-07 12:07:01 +0000

    Disable broken response time

--- middleware/response-time.js
+++ middleware/response-time.js
@@ -1,16 +1,16 @@
 import responseTime from 'response-time'
 import StatsD from 'node-statsd'
 
-let stats = new StatsD({
-  host: 'stats.tala.is',
-})
+// let stats = new StatsD({
+//   host: 'stats.tala.is',
+// })
 
 export default responseTime((req, res, time) => {
   try {
     let stat = (req.method + req.route.path).toLowerCase()
       .replace(/[:\.]/g, '')
       .replace(/\//g, '_')
-    stats.timing(stat, time)
+    // stats.timing(stat, time)
   } catch (err) {
     console.log(err)
   }
